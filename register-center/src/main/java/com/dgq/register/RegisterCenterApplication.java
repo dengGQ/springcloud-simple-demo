e26997c900d378b1f0cbd46f5cad2eeeafa1cc6c
dengGQ <565820745@qq.com>
2019-06-12 15:09:40 +0800

    去除无用package

--- register-center/src/main/java/com/dgq/register/RegisterCenterApplication.java
+++ register-center/src/main/java/com/dgq/register/RegisterCenterApplication.java
@@ -1,9 +1,7 @@
 package com.dgq.register;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
